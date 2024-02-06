4ca1ea2a68cb0a6530d98977abaff4101a45ca47
Agnė <139142864+Agne-Jazdauskiene@users.noreply.github.com>
2024-02-06 17:13:41 +0200

    2024-02-06_masyvu-aiskinimasis

--- 2024-02-06_perziurim_array/array-tasks2.js
+++ 2024-02-06_perziurim_array/array-tasks2.js
@@ -137,15 +137,119 @@ console.log(`-------------- J -------------`);
 const arrayEnd = [];
 for (let i = 0; i < array.length; i++) {
   if (i % 2 !== 0) {
+    //true
     arrayEnd.push(array[i]);
+    // arrayEnd.splice(i, 1);// splice netinka cia
+    // i--;
   }
 }
 console.log(arrayEnd);
 
 //SPLICE PVZ KAIP ISTRINTUME IS MASYVO VISUS ELEMENTUS. Kai istriname eiksme is pozicijos - tuomet pozicija pasislenka, o inkrementas lieka toje pacioje. Todel reikia naudoti dekrementa i--.
-const array5 = [2, 4, 6, 8];
-for (let i = 0; i < array5.length; i++) {
-  array5.splice(i, 1);
-  i--; // sugrizta perviena atgal (kuri istryne)
+// const array5 = [2, 4, 6, 8];
+// for (let i = 0; i < array5.length; i++) {
+//   array5.splice(i, 1);
+//   i--; // sugrizta perviena atgal (kuri istryne)
+// }
+// console.log(array5);
+
+console.log(`-------------- 3 UZDUOTIS -------------`);
+// Sugeneruokite du masyvus, kurių reikšmės yra atsitiktiniai skaičiai nuo 100 iki 999. Masyvų ilgiai 100. Masyvų reikšmės turi būti unikalios savo masyve (t.y. neturi kartotis).
+
+const arrayOne = [];
+const arrayTwo = [];
+
+for (let i = 0; arrayOne.length < 100; i++) {
+  // 1. pirma iteracija = []
+  // 2 iteracija = [150]
+  // 3. iteracija = [150, 180]
+
+  let numbers999 = rand(100, 999);
+  // console.log(arrayOne.includes(numbers999));
+
+  if (!arrayOne.includes(numbers999))
+    // ! reiskia - priesinga reiksme
+    arrayOne.push(numbers999);
+
+  // for (let x = 0; x < arrayOne.length; x++) { // sitokiu budu nesupratome :)
+  //   console.log(arrayOne[x]);
+  // }
 }
-console.log(array5);
+console.log(arrayOne);
+
+for (let i = 0; arrayTwo.length < 100; i++) {
+  let numbers999 = rand(100, 999);
+  if (!arrayTwo.includes(numbers999))
+    // ! reiskia - priesinga reiksme
+    arrayTwo.push(numbers999);
+}
+console.log(arrayTwo);
+
+console.log(`-------------- 4 UZDUOTIS -------------`);
+// Sugeneruokite masyvą, kuris būtų sudarytas iš reikšmių, kurios yra pirmame 3 uždavinio masyve, bet nėra antrame 3 uždavinio masyve.
+
+const array4uzd = [];
+
+for (let i = 0; i < arrayOne.length; i++) {
+  if (!arrayTwo.includes(arrayOne[i])) array4uzd.push(arrayOne[i]);
+}
+console.log(array4uzd);
+// console.log(array4uzd);
+
+console.log(`-------------- 5 UZDUOTIS -------------`);
+// Sugeneruokite masyvą iš elementų, kurie kartojasi abiejuose 3 uždavinio masyvuose.
+
+const array5uzd = [];
+
+for (let i = 0; i < arrayOne.length; i++) {
+  if (arrayTwo.includes(arrayOne[i])) array5uzd.push(arrayOne[i]);
+}
+
+console.log(array5uzd);
+
+console.log(`-------------- 6 UZDUOTIS -------------`);
+// Sugeneruokite 10 skaičių masyvą pagal taisyklę: Du pirmi skaičiai- atsitiktiniai nuo 5 iki 25. Trečias, pirmo ir antro suma. Ketvirtas- antro ir trečio suma. Penktas trečio ir ketvirto suma ir t.t. pvz. [10, 15, 25, 40, 65…]
+
+const array6uzd = [];
+array6uzd[0] = rand(5, 25);
+array6uzd[1] = rand(5, 25);
+// arba constt array6uzd = [rand(5, 25), rand(5, 25)]
+
+for (let i = 2; i < 10; i++) {
+  // console.log(array6uzd[i - 1]);
+
+  // console.log(array6uzd[0]);
+  console.log(array6uzd[0], array6uzd[1]);
+
+  array6uzd[2] = array6uzd[0] + array6uzd[1];
+  // //   i
+  console.log(array6uzd[2]);
+
+  array6uzd[i + 1] = array6uzd[i] + array6uzd[i - 1];
+  console.log(array6uzd[i + 1]);
+}
+console.log(array6uzd);
+
+// Paprastesnis variantas - bernioku
+// const unoArray = [rand(5, 25), rand(5, 25)];
+
+// for (let i = 2; i < 10; i++) {
+//   unoArray[i] = unoArray[i - 1] + unoArray[i - 2];
+// }
+// console.log(unoArray);
+
+console.log(`-------------- 7 UZDUOTIS -------------`);
+// Sugeneruokite masyvą, kurio reikšmės atsitiktinės raidės A, B, C ir D, o ilgis 200. Suskaičiuokite kiek yra kiekvienos raidės.
+
+const array7uzd = [];
+let letters = ["A", "B", "C", "D"];
+// const suma = 0;
+let randLetters = [];
+letters[rand(0, 3)];
+console.log(Letters);
+
+for (i = 0; i < 200; i++) {
+  let oneLetter = letters[rand(0, 3)];
+
+  randletters.push(oneLetter); // ?
+}
