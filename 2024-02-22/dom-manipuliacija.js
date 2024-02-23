3d33e5b4875adf5f31b47afdbc065b15cf289fdb
Agnė <139142864+Agne-Jazdauskiene@users.noreply.github.com>
2024-02-23 09:03:27 +0200

    2024-02-22_dom-cart

--- 2024-02-22/dom-manipuliacija.js
+++ 2024-02-22/dom-manipuliacija.js
@@ -20,41 +20,46 @@ const result = data.products.map((value) => {
   // returno ir riestiniu skl nereikia, nes turime viena eilute ir grazinimo nereikia
 
   return `<div class="row">
-  <div class="col-auto image">
-    <img src="${value.thumbnail}">
-  </div>
-  <div class="col info">
-      <h3>${value.title}</h3>
-      <div class="rating">
-      
-        <div class="gray-stars">
-          ${`<i class="bi bi-star-fill"></i>`.repeat(5)}
-        </div>
-
-        <div class="yellow-stars" 
-        style="width: ${calculateWidth(value.rating, 80, 5)}px">
-              <div class="stars-container">
-                ${`<i class="bi bi-star-fill"></i>`.repeat(5)}
+              <div class="col-auto image">
+              <img src="${value.thumbnail}">
               </div>
-      </div>
 
-      <div class="description pt-5">${value.description}</div>
-   </div>
-   <div class="col-auto checkout">
-      <div class="price mb-3">
-      ${
-        value.discountPercentage === 0
-          ? `   
-      <span class="original-price">&dollar;${value.price}</span>`
-          : `<span class="discount-price">&dollar;${discountPrice}</span>
-      <span class="regular-price">&dollar;${value.price}</span>
+            <div class="col info">
+              <h3>${value.title}</h3>
+
+              <div class="rating">
+                <div class="gray-stars">
+                  ${`<i class="bi bi-star-fill"></i>`.repeat(5)}
+                </div>
+
+                <div class="yellow-stars" style="width: ${calculateWidth(
+                  value.rating,
+                  80,
+                  5
+                )}px">
+                  <div class="stars-container">
+                    ${`<i class="bi bi-star-fill"></i>`.repeat(5)}
+                  </div>
+                </div>
+              </div>
+
+            <div class="description pt-5">${value.description}</div>
+          </div>
+          <div class="col-auto checkout">
+              <div class="price mb-3">
+              ${
+                value.discountPercentage === 0
+                  ? `   
+                  <span class="original-price">&dollar;${value.price}</span>`
+                  : `<span class="discount-price">&dollar;${discountPrice}</span>
+                  <span class="regular-price">&dollar;${value.price}</span>
       `
-      }
-      </div> 
-      <a class="btn btn-warning" onclick="addToCart(${
-        value.id
-      })" >Add to Cart</a>
-   </div>
+              }
+              </div> 
+          <a class="btn btn-warning" onclick="addToCart(${
+            value.id
+          })" >Add to Cart</a>
+          </div>
 </div>`;
 }); // value yra objektas
 // console.log(result);
@@ -65,16 +70,27 @@ const cartProducts = [];
 const addToCart = (id) => {
   // e.prevent.Default
   const product = data.products.find((el) => el.id === id);
-  cartProducts.push({
-    id: product.id,
-    title: product.title,
-    thumbnail: product.thumbnail,
-    price: product.price,
-    discountPercentage: product.discountPercentage,
-    qty: 1,
-  });
+  const cartIndex = cartProducts.findIndex((el) => el.id === id);
 
+  if (cartIndex > -1) {
+    //atnaujinimas
+    cartProducts[cartIndex].qty++;
+  } else {
+    //pridejimas
+    cartProducts.push({
+      id: product.id,
+      title: product.title,
+      thumbnail: product.thumbnail,
+      price: product.price,
+      discountPercentage: product.discountPercentage,
+      qty: 1,
+    });
+  }
   console.log(cartProducts);
+
+  document.querySelector(".result").style.display = `none`;
+  document.querySelector(".cart").style.display = `block`;
 };
 
-const object = data.products.find((el) => el.id === id);
+// const object = data.products.find((el) => el.id === id);
+// atnaujina , po to prideda
