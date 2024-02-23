25a1a73312b908e9105173703eabceaca9db5f4c
Agnė <139142864+Agne-Jazdauskiene@users.noreply.github.com>
2024-02-23 15:35:23 +0200

    2024-02-23_jason_fetch

---
+++ 2024-02-23/uzduotys/json/task-3-bitcoin.js
@@ -0,0 +1,27 @@
+// const remapData = (data) => {
+//   const bpi = data.bpi;
+// };
+
+// fetch("https://api.coindesk.com/v1/bpi/currentprice.json")
+//   .then((response) => response.json()) //Turinio konvertavimo eilutė
+
+//   .then((data) => remapData(data));
+
+// .then((response) => {
+//   document.querySelector(".usd").innerHTML = `${atsakymas.time}`;
+//   console.log(time);
+// }); //Grąžinamas atsakymas
+
+const bitToEur = () => {
+  fetch("https://api.coindesk.com/v1/bpi/currentprice.json")
+    .then((response) => response.json())
+    .then(
+      (data) =>
+        (document.querySelector(
+          ".bitToEur"
+        ).innerHTML = `1bitcoin = ${data.bpi.EUR.rate} Eur`).innerHTML = `1bitcoin = ${data.bpi.USD.rate} Eur`)
+    );
+   
+};
+
+
