53104a0314b4f22551ce2ada21c454e21d4b2a9e
Agnė <139142864+Agne-Jazdauskiene@users.noreply.github.com>
2024-03-08 12:30:23 +0200

    css.modules_router

---
+++ 2024-03-08_css-module_/treatwell-project/src/App.jsx
@@ -0,0 +1,42 @@
+// Treatwell
+
+// Sukurkite kirpėjų svetainę pasinaudodami react biblioteka. Duomenys saugomi localStorage
+// Aplikacijoje yra dvi sritys:
+// Administracinė ir Vieša
+
+// Viešoje srityje vartotojas gali matyti kirpėjų sąrašą kuriame atvaizduojama informacija:
+// Nuotrauka,
+// Vardas,
+// Telefono numeris
+
+// Paspaudus ant norimo žmogaus atvaizduojama informacija:
+// Nuotrauka,
+// Vardas,
+// Telefono numeris,
+// El. paštas,
+// Valandinis įkainis
+// Įvertinimas
+// Aprašymas
+
+// Administracinėje srityje administratorius taipogi mato kirpėjų sąrašą. Sukurkite funkcionalumą leidžiantį administratoriui:
+// Pridėti naują kirpėją,
+// Redaguoti esamą kirpėją,
+// Ištrinti kirpėją
+
+// Prieš ištrynimą vartotojas turi matyti patvirtinimo žinutę:
+// https://developer.mozilla.org/en-US/docs/Web/API/Window/confirm
+
+// PAPILDOMAI:
+// Viešoje srityje sukurkite filtrą leidžiantį ieškoti kirpėjo pagal jo vardą.
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Treatwell</h1>
+    </>
+  );
+}
+
+export default App;
