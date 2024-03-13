25e69e965dd628f79822d752be6b696154e9df47
Agnė <139142864+Agne-Jazdauskiene@users.noreply.github.com>
2024-03-13 17:02:34 +0200

    mongo-db-posts

--- 2024-03-13_Mongodb/controller/posts.js
+++ 2024-03-13_Mongodb/controller/posts.js
@@ -23,9 +23,55 @@ import Post from "../model/posts.js";
 
 const router = Router();
 
+// GET METODU VISUS DĖTI PAČIAME VIRŠUJE
+
+// 6. Susigrąžinti visų įrašų kiekį kolekcijoje.
+router.get("/total_posts", async (req, res) => {
+  // const obj = await Post.find();
+  // res.send({ count: obj.length });
+  //ARBA
+  res.send({ count: await Post.countDocuments() });
+});
+
+//5.Peržiūrėti visus įrašus
+router.get("/all_posts", async (req, res) => {
+  // Visų įrašų grąžinimas
+  res.send(await Post.find());
+});
+
+// 2 Peržiūrėti pasirinktą įrašą
+router.get("/:id", async (req, res) => {
+  // Vieno įrašo paėmimas
+
+  // Norint paimti parametro reikšmę: req.params
+  res.send(await Post.findById(req.params.id));
+});
+
+// 1 Sukurti naują įrašą
 router.post("/", async (req, res) => {
-  await Post.create(req.body);
-  res.send("Naujas įrašas");
+  //Data 1 var.
+  // await Post.create({ ...req.body, created_at: Date.now() });
+  //ARBA data - 2 var.
+  req.body.created_at = new Date();
+  const result = await Post.create(req.body);
+  // res.send("Naujas įrašas"); Grazina tik sia zinute
+  res.send({ message: "Irasas sukurtas", result });
+});
+
+// 3. Atnaujinti pasirinktą įrašą,
+// Įrašo atnaujinimas
+router.put("/:id", async (req, res) => {
+  await Post.findByIdAndUpdate(req.params.id, req.body);
+
+  res.send("Įrašas sėkmingai atnaujintas");
+});
+
+//4. Ištrinti norimą įrašą,
+// Įrašo ištrynimas
+router.delete("/:id", async (req, res) => {
+  await Post.findByIdAndDelete(req.params.id);
+
+  res.send("Įrašas sėkmingai ištrintas");
 });
 
 export default router;
