339e9745518174c79391b169d9101c8535234659
Agnė <139142864+Agne-Jazdauskiene@users.noreply.github.com>
2024-03-25 14:21:16 +0200

    create Registry project

---
+++ Final_Project/Registry/client/src/App.jsx
@@ -0,0 +1,34 @@
+import "./App.css";
+
+const App = () => {
+
+const
+
+
+  return <>
+  
+  // <MainContext.Provider value={contextValues}>
+  //       <BrowserRouter>
+  //         {/* TIKRINAME AR VARTOTOJAS YRA PRISIJUNGES */}
+  //         {user && (
+  //           <>
+  //             {/* Tikriname ar showNewPost reikšmė teigiama, jei taip atvaizduojame NewPost komponento modalinį langą */}
+  //             {showNewPost && <NewPost />}
+  //             <Sidebar />
+  //           </>
+  //         )}
+  //         <Routes>
+  //           {/* TIKRINAME AR VARTOTOJAS YRA PRISIJUNGES */}
+  //           {user && (
+  //             <>
+  //               <Route path="/" element={<Home />} />
+  //               <Route path="/user/:id" element={<User />} />
+  //             </>
+  //           )}
+  //           <Route path="/login" element={<Login />} />
+  //           <Route path="*" element={<Login />} />
+  //         </Routes>
+  //       </BrowserRouter>
+  //     </MainContext.Provider></>;
+};
+export default App;
